a1724b41b43971dc3839910f768e1567e9b5f2ed
v-lane <victoria.lane.m@gmail.com>
2024-03-29 17:36:39 -0400

    Add filter methods

--- filters.rb
+++ filters.rb
@@ -2,15 +2,58 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+  id_exists = nil
+  
+  @candidates.each do |candidate|
+    id_exists = candidate if candidate[:id] == id
   end
+
+  return id_exists
+end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    qualified_candidates = []
+    candidates.each do |candidate|
+      qualified_candidates << candidate if qualified_candidate?(candidate) 
+    end
+    return qualified_candidates
   end
   
   # More methods will go below
+
+  def minimum_github_points?(candidate)
+    candidate[:github_points] >= 100
+  end
+
+  def knows_python_or_ruby?(candidate)
+    candidate[:languages].include?("Python") || candidate[:languages].include?("Ruby")
+  end
+
+  def recent_application?(candidate)
+    (Date.today - candidate[:date_applied]) <= 15
+  end
+
+  def age_over_17?(candidate)
+    candidate[:age] >= 18
+  end
+
+  def qualified_candidate?(candidate)
+    experienced?(candidate) && 
+    minimum_github_points?(candidate) && 
+    knows_python_or_ruby?(candidate) && 
+    recent_application?(candidate) && 
+    age_over_17?(candidate)
+  end
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by!{|candidate| 
+      [
+        -1 * candidate[:years_of_experience],
+        -1 * candidate[:github_points]
+      ]
+    }
+  end
--- main.rb
+++ main.rb
@@ -6,4 +6,25 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+# pp @candidates[0]
+# pp @candidates[1]
+
+# puts experienced?(@candidates[1])
+
+# candidate0 = @candidates[0]
+# candidate1 = @candidates[1]
+
+# pp find(7)
+
+# pp experienced?(candidate)
+# pp minimum_github_points?(candidate)
+# pp knows_python_or_ruby?(candidate)
+# pp recent_application?(candidate)
+# pp age_over_17?(candidate)
+
+# pp qualified_candidate?(candidate0)
+# pp qualified_candidate?(candidate1)
+
+# pp qualified_candidates(@candidates)
+
+# pp ordered_by_qualifications(@candidates)
